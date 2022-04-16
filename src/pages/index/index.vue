150bee36782643803de436eeac9fbd3eeaba2bd5
Elton Zheng <elton.zheng.sh@gmail.com>
2022-04-16 17:11:35 +0800

    Initialize the project.

---
+++ src/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="container">
+    <view>
+      <text class="text-3xl text-neutral-600 m-6 text-center">{{
+        greeting
+      }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const greeting = ref('hello');
+</script>
